4c8dcbcfe7280e07cfa5a4cc371e4938e96e7c89
Sumit Jadiya <sumitjadiya@PUSJADIYA.local>
2019-08-21 20:28:47 +0530

    Code to print even from an AL

---
+++ src/com/appdirect/lambdas/OneParameterLambdaExample.java
@@ -0,0 +1,20 @@
+package com.appdirect.lambdas;
+
+@FunctionalInterface
+interface OneParameterLambdaInterface{
+	
+	public void displayValue(int x);
+	
+	default void normalDefaultMethod() {
+		System.out.println("That's why default methods were introduced from java 8");
+	}
+}
+
+public class OneParameterLambdaExample {
+
+	public static void main(String[] args) {
+		OneParameterLambdaInterface oneParameterLambdaInterface = (x)->System.out.println("The value is " + x);
+		oneParameterLambdaInterface.displayValue(10);
+		oneParameterLambdaInterface.normalDefaultMethod();
+	}
+}
---
+++ src/com/appdirect/lambdas/PrintEvenUsingLambdaExample.java
@@ -0,0 +1,21 @@
+package com.appdirect.lambdas;
+
+import java.util.ArrayList;
+
+public class PrintEvenUsingLambdaExample {
+
+    public static void main(String args[]) 
+    { 
+        ArrayList<Integer> arrL = new ArrayList<Integer>(); 
+        arrL.add(1); 
+        arrL.add(2); 
+        arrL.add(3); 
+        arrL.add(4); 
+  
+        arrL.forEach(n -> System.out.println(n)); 
+  
+        System.out.println("Even Numbers : ");
+        
+        arrL.forEach(n -> { if (n%2 == 0) System.out.println(n); }); 
+    } 
+} 

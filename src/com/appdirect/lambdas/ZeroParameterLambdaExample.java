6f1378c0da3c850a98fb0e69686521e4bc86c5e1
Sumit Jadiya <sumitjadiya@PUSJADIYA.local>
2019-08-21 14:57:54 +0530

    zero parameter lambda function

---
+++ src/com/appdirect/lambdas/ZeroParameterLambdaExample.java
@@ -0,0 +1,45 @@
+package com.appdirect.lambdas;
+
+@FunctionalInterface
+interface Interf{
+	
+	public void display();
+	
+    default void normalFun() 
+    { 
+       System.out.println("Hello"); 
+    } 
+}
+
+/*
+class ConcreteClass implements Interf{
+
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println("Through Concrete class");
+	}
+
+	public void local() {
+		// TODO Auto-generated method stub
+		System.out.println("local method");
+	}
+	
+}
+*/
+
+public class ZeroParameterLambdaExample {
+
+	public static void main(String[] args) {
+		
+		Interf interf = ()->System.out.println("Zero Parameter Lambda"); 
+		interf.display();
+
+/*		
+		ConcreteClass concreteClass = new ConcreteClass();
+		concreteClass.local();
+		concreteClass.display();
+*/
+	}
+	
+}
